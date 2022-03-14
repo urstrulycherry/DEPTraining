29013734689d27d9463430dc9728a60a4e7f5b2d
urstrulycharan <gcharannetha@gmail.com>
2022-03-14 17:33:55 +0530

    Updated Decorator

--- src/com/cherry/patterns/structural/Decorator.java
+++ src/com/cherry/patterns/structural/Decorator.java
@@ -7,7 +7,7 @@ interface Dress {
 class BasicDress implements Dress {
 	@Override
 	public void assemble() {
-		System.out.println("Basic Dress Features");
+		System.out.println("Basic Dress Features Added");
 	}
 }
 
